879bd882799c1a28d47f5747ac22d26ea97f2d0f
Warro <olli.warro@gmail.com>
2019-12-02 12:27:05 +0200

    refactoring

--- updater-api/src/index.js
+++ updater-api/src/index.js
@@ -1,21 +1,6 @@
 const { stan, opts, UPDATER_API_CHANNEL } = require('./utils/stan')
-const { sadd, smembers } = require('./utils/redis')
-const educationSearchQuery = require('./queries/educationSearch')
-
-const testQuery = async () => {
-  const response = await educationSearchQuery({ fullTextQuery: 'Tietojenkäsittelytietee' })
-  console.log('response', response)
-}
-
-const init = async () => {
-  await sadd('time', `Initialized at ${new Date()}`)
-  const times = await smembers('time')
-  console.log(times)
-  await testQuery()
-}
 
 stan.on('connect', async () => {
-  init()
   const sub = stan.subscribe(UPDATER_API_CHANNEL, 'updater-api.workers', opts)
   sub.on('message', msg => {
     console.log(JSON.parse(msg.getData()))
