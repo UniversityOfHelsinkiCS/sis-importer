013e89fbd398a6ccec9a10a9f50421b245084fd5
Warro <olli.warro@gmail.com>
2020-01-20 13:58:54 +0200

    added cron job for scheduling updater

--- importer-mankeli/src/index.js
+++ importer-mankeli/src/index.js
@@ -1,3 +1,4 @@
+const { REJECT_UNAUTHORIZED } = require('./config')
 const {
   stan,
   opts,
@@ -23,7 +24,7 @@ const { sleep } = require('./utils')
 const { onCurrentExecutionHashChange } = require('./utils/redis')
 const { connection, sequelize } = require('./db/connection')
 
-if (process.env.NODE_ENV === 'development') {
+if (REJECT_UNAUTHORIZED) {
   process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
 }
 
