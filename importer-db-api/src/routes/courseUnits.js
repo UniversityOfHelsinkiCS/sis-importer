d1b000cd981b0f21f6bf5f4314a5ad91beb375aa
Jami Kousa <jami.kousa@helsinki.fi>
2020-06-08 16:48:41 +0300

    test ci 2

--- importer-db-api/src/routes/courseUnits.js
+++ importer-db-api/src/routes/courseUnits.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
   const courseUnits = await models.CourseUnit.findOne()
-  const safe = [{ name: courseUnits.name, code: courseUnits.code }] // safe when testing ci
+  const safe = [{ name: courseUnits.name }] // safe when testing ci
   res.send(safe)
 })
 
