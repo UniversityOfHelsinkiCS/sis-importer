18fc90d5696e6ff3585bf4627abd1765e933aba3
Kalle Ilves <kalleilv@gmail.com>
2020-06-11 17:58:19 +0300

    Separate course unit realisations api

--- importer-db-api/src/routes/courseUnitRealisations.js
+++ importer-db-api/src/routes/courseUnitRealisations.js
@@ -1,9 +1,42 @@
 const express = require('express')
+const { Op } = require('sequelize') 
+
 const models = require('../models')
+const { NotFoundError } = require('../errors')
+
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-  const courseUnitRealisations = await models.CourseUnitRealisation.findOne()
+  const { code } = req.query
+
+  const courseUnit = await models.CourseUnit.findOne({
+    where: {
+      code,
+    },
+  })
+
+  if (!courseUnit) {
+    throw new NotFoundError(`Course unit with code ${code} is not found`)
+  }
+
+  const { groupId } = courseUnit
+
+  const assessmentItem = await models.AssessmentItem.findOne({
+    where: {
+      primary_course_unit_group_id: groupId,
+    },
+  })
+
+  const { id: assessmentItemId } = assessmentItem
+
+  const courseUnitRealisations = await models.CourseUnitRealisation.findAll({
+    where: {
+      assessmentItemIds: {
+        [Op.contains]: [assessmentItemId],
+      },
+    },
+  })
+
   res.send(courseUnitRealisations)
 })
 
--- importer-db-api/src/routes/courseUnits.js
+++ importer-db-api/src/routes/courseUnits.js
@@ -1,45 +1,9 @@
 const express = require('express')
 const { Op } = require('sequelize')
-const _ = require('lodash')
 
 const models = require('../models')
-const { NotFoundError } = require('../errors')
 
 const router = express.Router()
-
-router.get('/:code/course_unit_realisations', async (req, res) => {
-  const { code } = req.params
-
-  const courseUnit = await models.CourseUnit.findOne({
-    where: {
-      code,
-    },
-  })
-
-  if (!courseUnit) {
-    throw new NotFoundError(`Course unit with code ${code} is not found`)
-  }
-
-  const { groupId } = courseUnit
-
-  const assessmentItem = await models.AssessmentItem.findOne({
-    where: {
-      primary_course_unit_group_id: groupId,
-    },
-  })
-
-  const { id: assessmentItemId } = assessmentItem
-
-  const courseUnitRealisations = await models.CourseUnitRealisation.findAll({
-    where: {
-      assessmentItemIds: {
-        [Op.contains]: [assessmentItemId],
-      },
-    },
-  })
-
-  res.send(courseUnitRealisations)
-})
 
 router.get('/', async (req, res) => {
   const { codes: codesString } = req.query
@@ -63,17 +27,7 @@ router.get('/', async (req, res) => {
     },
   })
 
-  const groupedCourseUnits = _.groupBy(courseUnits, ({ code }) => code)
-
-  const groupedUniqueCourseUnits = _.mapValues(groupedCourseUnits, courseUnitsByCode =>
-    _.maxBy(courseUnitsByCode, ({ validityPeriod }) =>
-      validityPeriod && validityPeriod.endDate ? new Date(validityPeriod.endDate) : undefined
-    )
-  )
-
-  const uniqueCourseUnits = _.values(groupedUniqueCourseUnits);
-
-  res.send(uniqueCourseUnits)
+  res.send(courseUnits)
 })
 
 module.exports = router
