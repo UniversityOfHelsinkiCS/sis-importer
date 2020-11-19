ea115792a9336553967ecd7fe328e89ee88c60db
Roni Lindholm <roni.lindholm@helsinki.fi>
2020-11-19 11:54:09 +0200

    Add api key to Sis requests if it is present in environment

--- importer-api/src/utils/koriApi.js
+++ importer-api/src/utils/koriApi.js
@@ -2,9 +2,15 @@ const axios = require('axios').default
 const fs = require('fs')
 const https = require('https')
 const { request } = require('./index')
-const { SIS_API_URL, PROXY_TOKEN, KEY_PATH, CERT_PATH } = process.env
+const { SIS_API_URL, PROXY_TOKEN, KEY_PATH, CERT_PATH, API_KEY } = process.env
 
 const hasCerts = KEY_PATH && CERT_PATH
+
+const getHeaders = () => {
+  if (!hasCerts) return { token: PROXY_TOKEN }
+  else if (hasCerts && API_KEY) return { 'X-Api-Key': API_KEY }
+  return {}
+}
 
 const agent = hasCerts
   ? new https.Agent({
@@ -13,11 +19,9 @@ const agent = hasCerts
     })
   : new https.Agent()
 
-const headers = hasCerts ? {} : { token: PROXY_TOKEN }
-
 const koriInstance = axios.create({
   baseURL: `${SIS_API_URL}/kori/api`,
-  headers,
+  headers: getHeaders(),
   httpsAgent: agent
 })
 
