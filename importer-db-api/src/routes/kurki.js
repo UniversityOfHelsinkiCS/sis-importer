d698136352af895d6919e3646eff82c8c3051184
Kalle Ilves <kalleilv@gmail.com>
2021-01-11 14:47:41 +0200

    course unit realisations by programme

--- importer-db-api/src/routes/kurki.js
+++ importer-db-api/src/routes/kurki.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const _ = require('lodash')
 
 const models = require('../models')
 const { NotFoundError } = require('../errors')
+const { Op } = require('sequelize')
 
 const router = express.Router()
 
@@ -22,6 +24,63 @@ router.get('/students/:id', async (req, res) => {
   }
 
   res.send(student)
+})
+
+router.get('/course_unit_realisations/programme/:programmeCode', async (req, res) => {
+  const { activityPeriodEndDateAfter } = req.query
+  const { programmeCode } = req.params
+
+  const organisation = await models.Organisation.findOne({
+    where: {
+      code: programmeCode,
+    },
+  })
+
+  if (!organisation) {
+    throw new NotFoundError('No such organization, use different code e.g. 500-K005')
+  }
+
+  const courses = await organisation.getCourses()
+
+  const groupIds = courses.map(c => c.groupId)
+
+  const assessmentItems = await models.AssessmentItem.findAll({
+    where: {
+      primary_course_unit_group_id: {
+        [Op.in]: groupIds,
+      },
+    },
+  })
+
+  const validAssessmentItems = assessmentItems.filter(item => item.id.includes('default-teaching-participation'))
+
+  if (validAssessmentItems.length === 0) {
+    return res.send([])
+  }
+
+  const assessmentItemIds = _.uniq(validAssessmentItems.map(a => a.id))
+
+  const courseUnitRealisations = await models.CourseUnitRealisation.findAll({
+    where: {
+      assessmentItemIds: {
+        [Op.overlap]: assessmentItemIds
+      },
+      ...(activityPeriodEndDateAfter && {
+        [Op.and]: [
+          { activityPeriod: { endDate: { [Op.ne]: null } } },
+          {
+            activityPeriod: {
+              endDate: {
+                [Op.gt]: dateFns.format(new Date(activityPeriodEndDateAfter), 'yyyy-MM-dd'),
+              },
+            },
+          },
+        ],
+      }),
+    },
+  })
+
+  res.send(courseUnitRealisations)
 })
 
 module.exports = router
