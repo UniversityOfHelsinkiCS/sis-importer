b876255a6fac434320cf83ebd7b5efda3f167325
Roni Lindholm <roni.lindholm@helsinki.fi>
2021-02-09 09:17:04 +0200

    Add functionality to retrieve all grade scales

--- importer-db-api/src/routes/grades.js
+++ importer-db-api/src/routes/grades.js
@@ -8,18 +8,20 @@ const router = express.Router()
 router.get('/', async (req, res) => {
     const { codes } = req.query
 
+    const query = {
+        id: {
+            [Op.in]: Array.isArray(codes) ? codes : [codes]
+        }
+    }
+
     const gradeScales = await models.GradeScale.findAll({
-        where: {
-            id: {
-                [Op.in]: Array.isArray(codes) ? codes : [codes]
-            }
-        },
+        where: codes ? query : {},
         raw: true
     })
 
     if (!gradeScales.length) return res.status(404).send(`No grade scales found`)
 
-    const gradesByGradeScaleId = gradeScales.reduce((acc, {id, grades}) => {
+    const gradesByGradeScaleId = gradeScales.reduce((acc, { id, grades }) => {
         acc[id] = grades
         return acc
     }, {})
@@ -34,7 +36,7 @@ router.get('/:id', async (req, res) => {
     const { id } = req.params
 
     const gradeScale = await models.GradeScale.findOne({
-        where: {id }
+        where: { id }
     })
 
     if (!gradeScale) return res.status(404).send(`Garde scale with id ${id} does not exist`)
