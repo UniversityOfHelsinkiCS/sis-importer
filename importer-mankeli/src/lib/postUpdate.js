5fc273da77bb47eb5c6e1ae0fcde4aeea667ecda
Jami Kousa <jami.kousa@helsinki.fi>
2021-03-11 11:55:21 +0200

    postupdate remove del attainments without courses

--- importer-mankeli/src/lib/postUpdate.js
+++ importer-mankeli/src/lib/postUpdate.js
@@ -1,8 +1,6 @@
 const { stan, opts } = require('../utils/stan')
 const { NATS_GROUP, IS_DEV } = require('../config')
 
-const { connection } = require('../db/connection')
-
 const sendToStan = (channel, message) =>
   new Promise((res, rej) => {
     stan.publish(channel, JSON.stringify(message), err => {
@@ -11,17 +9,10 @@ const sendToStan = (channel, message) =>
     })
   })
 
-const clearCourseUnitAttainmentsWithoutCourseUnits = async () => {
-  return connection.sequelize.query(
-    `DELETE FROM attainments WHERE attainments.type = 'CourseUnitAttainment' AND NOT EXISTS (SELECT 1 FROM course_units where course_units.id = attainments.course_unit_id)`
-  )
-}
 
 const postUpdate = async () => {
   if (IS_DEV) return console.log('Skipping post update in dev env')
   console.log('Post Update Start')
-
-  await clearCourseUnitAttainmentsWithoutCourseUnits()
 
   sendToStan('POST_UPDATE_FINISH', { success: true })
 }
