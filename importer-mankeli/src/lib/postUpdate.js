4d39a694c5e8283ef2aae25e2b1a7efffcd1502b
Jami Kousa <jami.kousa@helsinki.fi>
2021-03-11 12:47:15 +0200

    postupdate delete open university studyrights

--- importer-mankeli/src/lib/postUpdate.js
+++ importer-mankeli/src/lib/postUpdate.js
@@ -1,6 +1,8 @@
 const { stan, opts } = require('../utils/stan')
 const { NATS_GROUP, IS_DEV } = require('../config')
 
+const { connection } = require('../db/connection')
+
 const sendToStan = (channel, message) =>
   new Promise((res, rej) => {
     stan.publish(channel, JSON.stringify(message), err => {
@@ -9,9 +11,17 @@ const sendToStan = (channel, message) =>
     })
   })
 
+const removeOpenUniversityStudyRights = async () => {
+  return connection.sequelize.query(
+    `DELETE FROM studyrights WHERE studyrights.education_id IN (SELECT id FROM educations WHERE educations.education_type = 'urn:code:education-type:non-degree-education:open-university-studies');`
+  )
+}
+
 const postUpdate = async () => {
   if (IS_DEV) return console.log('Skipping post update in dev env')
   console.log('Post Update Start')
+
+  await removeOpenUniversityStudyRights()
 
   sendToStan('POST_UPDATE_FINISH', { success: true })
 }
