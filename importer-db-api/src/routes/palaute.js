bee721949a3bc011e892cb8293de0775c7bd0ff0
Jami Kousa <jami.kousa@helsinki.fi>
2021-04-09 14:20:01 +0300

    palaute apis

--- importer-db-api/src/routes/palaute.js
+++ importer-db-api/src/routes/palaute.js
@@ -2,6 +2,13 @@ const { Op } = require('sequelize')
 const express = require('express')
 const models = require('../models')
 
+const relevantAttributes = {
+  enrolment: ['id', 'personId', 'assessmentItemId', 'courseUnitRealisationId', 'courseUnitId', 'studySubGroups'],
+  courseUnit: ['id', 'groupId', 'code', 'organisations', 'completionMethods', 'responsibilityInfos', 'name'],
+  courseUnitRealisation: ['id', 'name', 'nameSpecifier', 'assessmentItemIds', 'activityPeriod', 'courseUnitRealisationTypeUrn', 'studyGroupSets', 'organisations', 'responsibilityInfos'],
+  assessmentItem: ['id', 'name', 'nameSpecifier', 'assessmentItemType', 'organisations', 'primaryCourseUnitGroupId'],
+}
+
 const router = express.Router()
 
 router.get('/enrolled/:personId', async (req, res) => {
@@ -19,7 +26,26 @@ router.get('/enrolled/:personId', async (req, res) => {
     where: {
       personId,
     },
-    include: [{ model: models.CourseUnitRealisation.scope(scopes) }, { model: models.AssessmentItem }],
+    attributes: relevantAttributes.enrolment,
+    include: [
+      {
+        model: models.CourseUnitRealisation.scope(scopes),
+        attributes: relevantAttributes.courseUnitRealisation,
+        as: 'courseUnitRealisation',
+      },
+      {
+        model: models.AssessmentItem,
+        attributes: relevantAttributes.assessmentItem,
+        as: 'assessmentItem',
+        include: [
+          {
+            model: models.CourseUnit,
+            attributes: relevantAttributes.courseUnit,
+            as: 'courseUnit',
+          },
+        ],
+      },
+    ],
   })
 
   res.send(enrolments)
@@ -30,7 +56,8 @@ router.get('/responsible/:personId', async (req, res) => {
     params: { personId },
   } = req
 
-  const courses = await models.CourseUnitRealisation.findAll({
+  const courseUnitRealisations = await models.CourseUnitRealisation.findAll({
+    attributes: relevantAttributes.courseUnitRealisation,
     where: {
       responsibilityInfos: {
         [Op.contains]: [
@@ -42,24 +69,37 @@ router.get('/responsible/:personId', async (req, res) => {
     },
   })
 
-  const assessmentItemIds = [].concat(...courses.map(c => c.assessmentItemIds))
+  const courseUnits = await models.CourseUnit.findAll({
+    attributes: relevantAttributes.courseUnit,
+    where: {
+      responsibilityInfos: {
+        [Op.contains]: [
+          {
+            personId: personId,
+          },
+        ],
+      },
+    },
+  })
+
+  const assessmentItemIds = [].concat(...courseUnitRealisations.map(c => c.assessmentItemIds))
 
   const assessmentItems = await models.AssessmentItem.findAll({
+    attributes: relevantAttributes.assessmentItem,
     where: {
       id: {
         [Op.in]: assessmentItemIds,
       },
     },
+    include: [{ model: models.CourseUnit, attributes: relevantAttributes.courseUnit, 
+      as: 'courseUnit' }],
   })
 
-  const withAssessmentItems = courses.map(course => {
-    return {
-      ...course.dataValues,
-      assessmentItems: course.assessmentItemIds.map(id => assessmentItems.find(aItem => aItem.id === id)),
-    }
+  res.send({
+    courseUnitRealisations,
+    courseUnits,
+    assessmentItems,
   })
-
-  res.send(withAssessmentItems)
 })
 
 module.exports = router
