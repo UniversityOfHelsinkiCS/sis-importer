3f0fb4e793bb4574f75fc1ba8974d8e3a0106c5d
Jami Kousa <jami.kousa@helsinki.fi>
2021-04-13 11:26:17 +0300

    "fix" archeology

--- importer-db-api/src/routes/archeology.js
+++ importer-db-api/src/routes/archeology.js
@@ -3,57 +3,67 @@ const models = require('../models')
 const { Op, QueryTypes } = require('sequelize')
 const { sequelize } = require('../config/db')
 
+const getEducationByIdForStudyright = async id => {
+  try {
+    const education = await models.Education.findOne({
+      where: { id },
+      raw: true,
+    })
+
+    delete education.organisations
+    delete education.responsibilityInfos
+    delete education.universityOrgIds
+    delete education.attainmentLanguages
+    delete education.studyFields
+    if (education.structure && education.structure.phase1)
+      education.structure.phase1 = { name: education.structure.phase1.name }
+    if (education.structure && education.structure.phase2)
+      education.structure.phase2 = { name: education.structure.phase2.name }
+    if (education.structure && education.structure.learningOpportunities)
+      delete education.structure.learningOpportunities
+
+    return education
+  } catch (err) {
+    console.log(err)
+    return { education: 'is missing from the database' }
+  }
+}
+
 router.get('/:studentNumber/studyrights', async (req, res) => {
   try {
     const student = await models.Person.findOne({
       where: {
         studentNumber: req.params.studentNumber,
-      }
+      },
     })
 
     const { openUni: includeOpenUni } = req.query
     const { deleted: includeDeleted } = req.query
 
-    if (!student)
-      return res.status(404).send('Student not found')
+    if (!student) return res.status(404).send('Student not found')
 
     const studyRights = await models.StudyRight.findAll({
       where: { personId: student.id },
       order: [['modificationOrdinal', 'DESC']],
-      raw: true
+      raw: true,
     })
     if (!studyRights.length) return res.json([])
 
     const mankeledStudyRights = []
     for (const studyRight of studyRights) {
       const { educationPhase1GroupId, educationPhase2GroupId } = studyRight.acceptedSelectionPath || {}
-      const education = await models.Education.findOne({
-        where: { id: studyRight.educationId },
-        raw: true
-      })
-      if (!includeDeleted && studyRight.documentState === 'DELETED')
-        continue
-      if (!includeOpenUni && education && education.educationType.includes('open-university-studies'))
-        continue
 
+      const education = await getEducationByIdForStudyright(studyRight.educationId)
 
-      delete education.organisations
-      delete education.responsibilityInfos
-      delete education.universityOrgIds
-      delete education.attainmentLanguages
-      delete education.studyFields
-      if (education.structure && education.structure.phase1)
-        education.structure.phase1 = { name: education.structure.phase1.name }
-      if (education.structure && education.structure.phase2)
-        education.structure.phase2 = { name: education.structure.phase2.name }
-      if (education.structure && education.structure.learningOpportunities)
-        delete education.structure.learningOpportunities
+      if (!includeDeleted && studyRight.documentState === 'DELETED') continue
+      if (!includeOpenUni && education?.educationType?.includes('open-university-studies')) continue
+
       const additionalData = { education }
 
       if (educationPhase1GroupId) {
         const educationPhase1 = await models.Module.findOne({
           where: { groupId: educationPhase1GroupId },
-          raw: true
+          raw: true,
         })
         if (educationPhase1) {
           delete educationPhase1.responsibilityInfos
@@ -68,7 +78,7 @@ router.get('/:studentNumber/studyrights', async (req, res) => {
       if (educationPhase2GroupId) {
         const educationPhase2 = await models.Module.findOne({
           where: { groupId: educationPhase2GroupId },
-          raw: true
+          raw: true,
         })
         if (educationPhase2) {
           delete educationPhase2.responsibilityInfos
@@ -79,12 +89,10 @@ router.get('/:studentNumber/studyrights', async (req, res) => {
           delete educationPhase2.rule
         }
         additionalData.educationPhase2 = educationPhase2
-
       }
       mankeledStudyRights.push({ ...studyRight, ...additionalData })
     }
     return res.json(mankeledStudyRights)
-
   } catch (e) {
     console.log(e)
     res.status(500).json(e.toString())
@@ -96,25 +104,25 @@ router.get('/assessments', async (req, res) => {
   try {
     const courseUnits = await models.CourseUnit.findAll({
       where: {
-        code: { [Op.iLike]: '%TKT%' }
+        code: { [Op.iLike]: '%TKT%' },
       },
-      raw: true
+      raw: true,
     })
     const assessmentItems = await models.AssessmentItem.findAll({
       where: {
         primaryCourseUnitGroupId: {
-          [Op.in]: courseUnits.map(({ groupId }) => groupId)
-        }
+          [Op.in]: courseUnits.map(({ groupId }) => groupId),
+        },
       },
-      raw: true
+      raw: true,
     })
     const realisations = await models.CourseUnitRealisation.findAll({
       where: {
         assessmentItemIds: {
-          [Op.overlap]: assessmentItems.map(({ id }) => id)
-        }
+          [Op.overlap]: assessmentItems.map(({ id }) => id),
+        },
       },
-      raw: true
+      raw: true,
     })
     console.log(`GOT ${realisations.length} realisations`)
     const wat = realisations.filter(r => r.assessmentItemIds.length > 1)
@@ -130,34 +138,31 @@ router.get('/:studentNumber/attainments', async (req, res) => {
     const student = await models.Person.findOne({
       where: {
         studentNumber: req.params.studentNumber,
-      }
+      },
     })
 
-    if (!student)
-      return res.status(404).send('Student not found')
+    if (!student) return res.status(404).send('Student not found')
 
     const attainments = await models.Attainment.findAll({
       where: {
-        personId: student.id
+        personId: student.id,
       },
-      raw: true
+      raw: true,
     })
 
     const attainmentsWithCourses = await Promise.all(
-      attainments.map(async (a) => {
+      attainments.map(async a => {
         const courseUnit = await models.CourseUnit.findOne({
           where: {
-            id: a.courseUnitId
-          }
+            id: a.courseUnitId,
+          },
         })
         return Promise.resolve({ ...a, courseUnit })
       })
     )
 
     if (req.query.code)
-      return res.send(attainmentsWithCourses.filter(a =>
-        a.courseUnit && a.courseUnit.code.includes(req.query.code)
-      ))
+      return res.send(attainmentsWithCourses.filter(a => a.courseUnit && a.courseUnit.code.includes(req.query.code)))
     return res.send(attainmentsWithCourses)
   } catch (e) {
     console.log(e)
@@ -170,26 +175,29 @@ router.get('/:studentNumber/enrollments', async (req, res) => {
     const student = await models.Person.findOne({
       where: {
         studentNumber: req.params.studentNumber,
-      }
+      },
     })
-    if (!student)
-      return res.status(404).send('Student not found')
+    if (!student) return res.status(404).send('Student not found')
 
     const enrolments = await models.Enrolment.findAll({
       where: {
-        personId: student.id
+        personId: student.id,
       },
       order: [['enrolmentDateTime', 'DESC']],
       limit: req.query.limit || null,
-      raw: true
+      raw: true,
     })
 
-    const mankeled = await Promise.all(enrolments.map(async (e) => {
-      const assessmentItem = await models.AssessmentItem.findOne({ where: { id: e.assessmentItemId } })
-      const courseUnit = await models.CourseUnit.findOne({ where: { id: e.courseUnitId } })
-      const courseUnitRealisation = await models.CourseUnitRealisation.findOne({ where: { id: e.courseUnitRealisationId } })
-      return Promise.resolve({ ...e, assessmentItem, courseUnit, courseUnitRealisation })
-    }))
+    const mankeled = await Promise.all(
+      enrolments.map(async e => {
+        const assessmentItem = await models.AssessmentItem.findOne({ where: { id: e.assessmentItemId } })
+        const courseUnit = await models.CourseUnit.findOne({ where: { id: e.courseUnitId } })
+        const courseUnitRealisation = await models.CourseUnitRealisation.findOne({
+          where: { id: e.courseUnitRealisationId },
+        })
+        return Promise.resolve({ ...e, assessmentItem, courseUnit, courseUnitRealisation })
+      })
+    )
     return res.send(mankeled)
   } catch (e) {
     console.log(e)
@@ -203,15 +211,13 @@ router.get('/assessments/:code', async (req, res) => {
       where: {
         code: req.params.code,
       },
-      raw: true
+      raw: true,
     })
-    if (!courseUnit)
-      return res.status(404).send('Course not found')
+    if (!courseUnit) return res.status(404).send('Course not found')
 
     const assessmentItems = await models.AssessmentItem.findAll({
       where: { primaryCourseUnitGroupId: courseUnit.groupId },
-      raw: true
-
+      raw: true,
     })
     return res.send({ courseUnit, assessmentItems })
   } catch (e) {
@@ -228,21 +234,22 @@ router.get('/:studentNumber/missing', async (req, res) => {
     const student = await models.Person.findOne({
       where: {
         studentNumber: req.params.studentNumber,
-      }
+      },
     })
-    if (!student)
-      return res.status(404).send('Student not found')
+    if (!student) return res.status(404).send('Student not found')
 
     if (cache[student.id]) {
       console.log('Data from cache')
       return res.send(cache[student.id])
     }
 
-    const data = await sequelize.query(`SELECT * FROM attainments
+    const data = await sequelize.query(
+      `SELECT * FROM attainments
             WHERE attainments.type = 'CourseUnitAttainment'
                 AND attainments.person_id = '${student.id}'
                 AND NOT EXISTS (SELECT 1 FROM course_units where course_units.id = attainments.course_unit_id)
-            `, { type: QueryTypes.SELECT }
+            `,
+      { type: QueryTypes.SELECT }
     )
 
     cache[student.id] = data
@@ -256,16 +263,14 @@ router.get('/:studentNumber/missing', async (req, res) => {
 router.post('/missing', async (req, res) => {
   try {
     const reqData = req.body
-    if (!Array.isArray(reqData))
-      return res.status(400).send({ error: 'Input should be an array' })
+    if (!Array.isArray(reqData)) return res.status(400).send({ error: 'Input should be an array' })
     const persons = await models.Person.findAll({
       where: {
-        studentNumber: { [Op.in]: reqData.map((studentNumber) => studentNumber) }
+        studentNumber: { [Op.in]: reqData.map(studentNumber => studentNumber) },
       },
-      raw: true
+      raw: true,
     })
-    if (!persons.length)
-      return res.status(404).send('Students not found')
+    if (!persons.length) return res.status(404).send('Students not found')
 
     const allPersonIds = persons.map(({ id }) => id)
     const output = Object.keys(cache)
@@ -277,15 +282,17 @@ router.post('/missing', async (req, res) => {
     const notCachedStudents = persons.filter(({ id }) => !(id in cache))
     const listOfStudents = notCachedStudents.map(({ id }) => `'${id}'`).join(', ')
     if (!listOfStudents) return res.send(output)
-    const data = await sequelize.query(`SELECT * FROM attainments
+    const data = await sequelize.query(
+      `SELECT * FROM attainments
             WHERE attainments.type = 'CourseUnitAttainment'
                 AND attainments.person_id IN (${listOfStudents})
                 AND NOT EXISTS (SELECT 1 FROM course_units where course_units.id = attainments.course_unit_id)
-            `, { type: QueryTypes.SELECT }
+            `,
+      { type: QueryTypes.SELECT }
     )
 
     // Hecking O(n^inf) way to do cache
-    notCachedStudents.forEach((person) => {
+    notCachedStudents.forEach(person => {
       const allAttainments = data.filter(attainment => attainment.person_id === person.id)
       cache[person.id] = allAttainments
     })
@@ -296,11 +303,11 @@ router.post('/missing', async (req, res) => {
   }
 })
 
-
 // Massive CPU and memory usage here
 // ⊂(◉‿◉)つ
 router.get('/possibleCoursesWithPartAttainments', async (req, res) => {
-  const courseUnits = await sequelize.query(`
+  const courseUnits = await sequelize.query(
+    `
   SELECT c_units.code, c_units.name, c_units.completion_methods, c_units.group_id, sub.id_max as id
     FROM (
         SELECT code, MAX(id) AS id_max
@@ -308,11 +315,13 @@ router.get('/possibleCoursesWithPartAttainments', async (req, res) => {
         GROUP BY code
     ) sub
     JOIN course_units c_units ON c_units.code = sub.code AND sub.id_max = c_units.id
-  `, {
-    mapToModel: true,
-    model: models.CourseUnit,
-    raw: true
-  })
+  `,
+    {
+      mapToModel: true,
+      model: models.CourseUnit,
+      raw: true,
+    }
+  )
 
   // hy-CM-117766481 <-- IT'S A MATCH!
   // hy-CM-117766481-default-teaching-participation <-- NO GOOD
@@ -326,17 +335,17 @@ router.get('/possibleCoursesWithPartAttainments', async (req, res) => {
     })
     .map(courseUnit => ({
       ...courseUnit,
-      completionMethods: courseUnit.completionMethods.find(m => !!re.exec(m.localId))
+      completionMethods: courseUnit.completionMethods.find(m => !!re.exec(m.localId)),
     }))
-  console.log("TOTAL", output.length)
+  console.log('TOTAL', output.length)
   return res.send(output)
-
 })
 
 // Archeology monster
 // 	༼ ༎ຶ ෴ ༎ຶ༽
 router.get('/assessmentItemsFromCoursesWithPartAttainments', async (req, res) => {
-  const courseUnits = await sequelize.query(`
+  const courseUnits = await sequelize.query(
+    `
   SELECT c_units.code, c_units.name, c_units.completion_methods, sub.id_max as id
     FROM (
         SELECT code, MAX(id) AS id_max
@@ -344,33 +353,39 @@ router.get('/assessmentItemsFromCoursesWithPartAttainments', async (req, res) =>
         GROUP BY code
     ) sub
     JOIN course_units c_units ON c_units.code = sub.code AND sub.id_max = c_units.id
-  `, {
-    mapToModel: true,
-    model: models.CourseUnit,
-    raw: true
-  })
+  `,
+    {
+      mapToModel: true,
+      model: models.CourseUnit,
+      raw: true,
+    }
+  )
 
   // hy-CM-117766481 <-- IT'S A MATCH!
   // hy-CM-117766481-default-teaching-participation <-- NO GOOD
   const re = /hy-CM-(\d*)$/
 
-  const assessmentItemIds = [...new Set(courseUnits
-    .filter(courseUnit => {
-      const theMethod = courseUnit.completionMethods.find(m => !!re.exec(m.localId))
-      if (!theMethod) return false
-      return theMethod.assessmentItemIds.length > 1
-    })
-    .map(courseUnit => ({
-      ...courseUnit,
-      completionMethods: courseUnit.completionMethods.find(m => !!re.exec(m.localId))
-    }))
-    .map(c => c.completionMethods.assessmentItemIds).flat()
-  )]
+  const assessmentItemIds = [
+    ...new Set(
+      courseUnits
+        .filter(courseUnit => {
+          const theMethod = courseUnit.completionMethods.find(m => !!re.exec(m.localId))
+          if (!theMethod) return false
+          return theMethod.assessmentItemIds.length > 1
+        })
+        .map(courseUnit => ({
+          ...courseUnit,
+          completionMethods: courseUnit.completionMethods.find(m => !!re.exec(m.localId)),
+        }))
+        .map(c => c.completionMethods.assessmentItemIds)
+        .flat()
+    ),
+  ]
 
   const output = await models.AssessmentItem.findAll({
-    where: {id: {[Op.in]: assessmentItemIds}},
+    where: { id: { [Op.in]: assessmentItemIds } },
     attributes: ['name', 'credits', 'id', 'primaryCourseUnitGroupId'],
-    raw: true
+    raw: true,
   })
   return res.send(output)
 })
