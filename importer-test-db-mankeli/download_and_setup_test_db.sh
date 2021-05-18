a7b7383e5fc4f24761cf90d245b2692c24615aee
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-05-18 15:08:52 +0300

    Add docker setup to script

--- importer-test-db-mankeli/download_and_setup_test_db.sh
+++ importer-test-db-mankeli/download_and_setup_test_db.sh
@@ -24,6 +24,28 @@ get_username() {
   username=$(head -n 1 < $USER_DATA_FILE_PATH)
 }
 
+drop_psql () {
+    echo "Dropping psql in container $1 with db name $2"
+    retry docker exec -u postgres $1 pg_isready --dbname=$2
+    docker exec -u postgres $1 dropdb $2 || echo "container $1 DB $2 does not exist"
+}
+
+ping_psql () {
+    drop_psql $1 $2
+    echo "Creating psql in container $1 with db name $2"
+    retry docker exec -u postgres $1 pg_isready --dbname=$2
+    docker exec -u postgres $1 createdb $2 || echo "container $1 DB $2 already exists"
+}
+
+restore_psql_from_backup () {
+    echo ""
+    echo "Restoring database from backup ($1/$2):"
+    echo "  1. Copying dump..."
+    docker cp $1 $2:/asd.sqz
+    echo "  2. Writing database..."
+    docker exec $2 pg_restore -U postgres --no-owner -F c --dbname=$3 -j4 /asd.sqz
+}
+
 run_importer_test_db_setup() {
     get_username
     echo "Using your Uni Helsinki username: $username"
@@ -32,6 +54,12 @@ run_importer_test_db_setup() {
     remotepath="/home/importer_user/staging/backup/importer-db-staging.sqz"
     scp -r -o ProxyCommand="ssh -l $username -W %h:%p melkki.cs.helsinki.fi" \
     "$username@importer:$remotepath" .
+
+    # Setup db inside docker
+    docker-compose up -d sis-importer-test-db
+    ping_psql "sis-importer-test-db" "importer-db"
+    restore_psql_from_backup "importer-db-staging.sqz" sis-importer-test-db importer-db
+    docker-compose down
 }
 
 # Run the script
