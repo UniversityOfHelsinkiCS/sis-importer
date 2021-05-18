cf9ba103131eb7b350608596960fdb370b57624e
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-05-18 18:02:06 +0300

    Add first version with population of students

--- importer-test-db-mankeli/src/index.js
+++ importer-test-db-mankeli/src/index.js
@@ -9,8 +9,25 @@ knexConnection.on('connect', async () => {
   console.log('Knex database connection established successfully')
 })
 
+const getStudentsWithRecentStudyrights = async () => {
+  const { knex } = knexConnection
+  const getNewerThanDate = '2017-01-01'
+  const nonDegreeEducations = knex.select('educations.id').from('educations')
+                                  .leftJoin('education_types', 'education_types.id', 'educations.education_type')
+                                  .where('parent_id', 'urn:code:education-type:non-degree-education')
+  
+  return await knex.select('student_number').from('persons')
+                             .leftJoin('studyrights', 'studyrights.person_id', 'persons.id')
+                             .where('grant_date', '>', getNewerThanDate)
+                             .whereNotIn('education_id', nonDegreeEducations)
+                             .distinct()
+}
+
 const run = async () => {
   await knexConnection.connect()
+
+  const students = await getStudentsWithRecentStudyrights()
+  console.log(students)
 }
 
 run()
