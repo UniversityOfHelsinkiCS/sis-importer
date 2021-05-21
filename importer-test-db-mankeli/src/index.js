f8a31e25b8fc82adedc8f803182cdaa6a1b876e5
Roni Lindholm <roni.lindholm@helsinki.fi>
2021-05-21 15:18:14 +0300

    Add more danger mangel

--- importer-test-db-mankeli/src/index.js
+++ importer-test-db-mankeli/src/index.js
@@ -1,5 +1,8 @@
 const { knexConnection } = require('./db/connection')
 
+// Danger zone
+const DESTROY = false
+
 knexConnection.on('error', e => {
   console.log('Knex database connection failed', e)
   process.exit(1)
@@ -27,27 +30,18 @@ const getPersonIdsOfStudentsWithRecentStudyRights = async () => {
     .distinctOn('student_number')).map(person => person.id)
 }
 
-const removeStudentsFromPersonsTable = async (students) => {
-  const { knex } = knexConnection
-  // replace select with del later
-  const deleted = await knex('persons').whereNotIn('id', students)
-  console.log(deleted.length)
-}
-
 const removeAttainments = async (students) => {
   const { knex } = knexConnection
-  //const deleted = await knex.select('attainments.id').from('attainments').whereNotIn('person_id', students)
   const relevantAttainmentsAndCourseUnits = await knex.select('id', 'course_unit_id').from('attainments').whereIn('person_id', students).whereNotNull('course_unit_id')
   const courseUnitGroupIds = await knex.select('group_id').from('course_units').whereIn('id', relevantAttainmentsAndCourseUnits.map(({course_unit_id}) => course_unit_id))
   const relevantAssessmentItems = await knex.select('id').from('assessment_items').whereIn('primary_course_unit_group_id', courseUnitGroupIds.map(({group_id}) => group_id))
   const relevantCourseUnitRealisations = await knex.select('id').from('course_unit_realisations').where('assessment_item_ids', '&&', relevantAssessmentItems.map(({id}) => id))
-  console.log(relevantAttainmentsAndCourseUnits.length, relevantAssessmentItems.length, relevantCourseUnitRealisations.length)
-}
-
-const removeEnrollments = async (students) => {
-  const { knex } = knexConnection
-  const relevantEnrollments = await knex.select('id').from('enrollments').whereIn('person_id', students)
-  console.log(relevantEnrollments.length)
+  await Promise.all([
+    removeStuff('attainments', 'id', relevantAttainmentsAndCourseUnits.map(({id}) => id), knex),
+    removeStuff('course_units', 'group_id', courseUnitGroupIds.map(({group_id}) => group_id), knex),
+    removeStuff('assessment_items', 'id', relevantAssessmentItems.map(({id}) => id), knex),
+    removeStuff('course_unit_realisations', 'id', relevantCourseUnitRealisations.map(({id}) => id), knex)
+  ])
 }
 
 const getRandomSampleOfSizeN = (students, n) => {
@@ -56,28 +50,46 @@ const getRandomSampleOfSizeN = (students, n) => {
 
 const removeStudyRightsAndOthersFromTables = async students => {
   const { knex } = knexConnection
-  // replace these selects with del later
-  const deletedStudyrights = await knex.select('*').from('studyrights').whereIn('person_id', students)
-  console.log(deletedStudyrights.length)
-  const deletedStudyRightPrimalities = await knex.select('*').from('study_right_primalities').whereIn('student_id', students)
-  console.log(deletedStudyRightPrimalities.length)
-  const deletedTermRegistrations = await knex.select('*').from('term_registrations').whereIn('student_id', students)
-  console.log(deletedTermRegistrations.length)
+  const relevantStudyRights = await knex.select('*').from('studyrights').whereIn('person_id', students)
+  const relevantModules = await knex.select('id').from('modules')
+    .whereIn(
+      'id',
+      relevantStudyRights.map(s => Object.keys(s.accepted_selection_path).map(k => s.accepted_selection_path[k])).flat()
+    )
+  await Promise.all([
+    removeStuff('studyrights', 'person_id', students, knex),
+    removeStuff('study_right_primalities', 'student_id', students, knex),
+    removeStuff('term_registrations', 'student_id', students, knex),
+    removeStuff('modules', 'id', relevantModules.map(({id}) => id), knex)
+  ])
+}
 
+/**
+  * Nuke stuff from db in table by preserving rows with given values in given column.
+  * @returns promise?
+*/
+const removeStuff = async (table, column, idsNotToDelete, knex) => {
+  if (DESTROY)
+    return knex(table).whereNotIn(column, idsNotToDelete).del()
+  const res = await knex(table).whereNotIn(column, idsNotToDelete).count(column)
+  console.log(`Would delete ${res[0].count} rows from ${table}`)
+  return Promise.resolve(res[0].count)
 }
 
 const run = async () => {
   await knexConnection.connect()
   const { knex } = knexConnection
 
-  const selected = getRandomSampleOfSizeN(await getPersonIdsOfStudentsWithRecentStudyRights(), 1000)
+  const selected = getRandomSampleOfSizeN(await getPersonIdsOfStudentsWithRecentStudyRights(), 100)
 
-  await removeAttainments(selected)
-  // await removeStudentsFromPersonsTable(selected)
-  // await removeStudyRightsAndOthersFromTables(selected)
+  removeStuff('persons', 'id', selected, knex)
+  removeStudyRightsAndOthersFromTables(selected)
+  removeStuff('enrolments', 'person_id', selected, knex)
+  removeAttainments(selected)
 
-  // TODO: remove modules
   // TODO: remove other non related tables
+
+  console.log("Data deleted successfully")
 }
 
 run()
