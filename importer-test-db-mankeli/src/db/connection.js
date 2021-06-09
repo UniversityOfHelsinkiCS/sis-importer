67b33024ede027290fec5b3219cf0d059b3142c1
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-06-09 23:31:37 +0300

    Change sample db to work with deletions instead

--- importer-test-db-mankeli/src/db/connection.js
+++ importer-test-db-mankeli/src/db/connection.js
@@ -1,30 +1,14 @@
 const knex = require('knex')
-const Sequelize = require('sequelize')
-
 
 const { DB_HOST, DB_PORT, DB_USERNAME, SOURCE_DATABASE, TARGET_DATABASE } = process.env
 
-DB_CONFIG = {
-  dialect: 'postgres',
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 10000,
-    idle: 300000000
-  },
-  username: DB_USERNAME,
-  port: DB_PORT,
-  host: DB_HOST,
-  database: TARGET_DATABASE,
-  logging: false
-}
-
 class Connection {
-  constructor() {
+  constructor(db) {
     this.RETRY_ATTEMPTS = 15
+    this.db = db
   }
 
-  async connectKnex(attempt = 1) {
+  async connect(attempt = 1) {
     try {
       this.knex = knex({
         client: 'pg',
@@ -32,7 +16,7 @@ class Connection {
           host: DB_HOST,
           port: DB_PORT,
           user: DB_USERNAME,
-          database: SOURCE_DATABASE
+          database: this.db
         },
         pool: {
           min: 0,
@@ -40,40 +24,26 @@ class Connection {
         }
       })
       await this.knex.raw('select 1+1 as result')
-      console.log("Connected to source db with knex")
+      console.log(`Connected to ${this.db}`)
     } catch (e) {
       if (attempt > this.RETRY_ATTEMPTS) {
-        console.log("Attempted too many times with knex, returning")
+        console.log(`Max retry attemps reached for ${this.db}, returning`)
         return
       }
-      console.log(`Knex database connection failed! Attempt ${attempt}/${this.RETRY_ATTEMPTS}`)
+      console.log(`Database connection to ${this.db} failed! Attempt ${attempt}/${this.RETRY_ATTEMPTS}`)
       console.log("Error: ", e)
       setTimeout(() => this.connectKnex(attempt + 1), 1000 * attempt)
     }
   }
-  async connectSequelize(attempt = 1) {
-    try {
-      this.sequelize = new Sequelize({
-        ...DB_CONFIG
-      })
-      await this.sequelize.authenticate()
-      console.log("Connected to target db with sequelize")
-    } catch (e) {
-      if (attempt > this.RETRY_ATTEMPTS) {
-        console.log("Attempted too many times with sequelize, returning")
-        return
-      }
-      console.log(`Sequelize database connection failed! Attempt ${attempt}/${this.RETRY_ATTEMPTS}`)
-      console.log("Error: ", e)
-      setTimeout(() => this.connectSequelize(attempt + 1), 1000 * attempt)
-    }
-  }
 }
 
-const connection = new Connection()
-connection.connectKnex(),
-connection.connectSequelize()
+const sourceConnection = new Connection(SOURCE_DATABASE)
+const targetConnection = new Connection(TARGET_DATABASE)
+
+const connections = [sourceConnection, targetConnection]
+connections.forEach(conn => conn.connect())
 
 module.exports = {
-  connection,
+  sourceConnection,
+  targetConnection
 }
--- importer-test-db-mankeli/src/index.js
+++ importer-test-db-mankeli/src/index.js
@@ -1,126 +1,102 @@
-const { connection } = require('./db/connection')
-const { bscStudents, mscStudents, otherStudents } = require('./studentNumbersForTestDump')
+const { sourceConnection, targetConnection } = require('./db/connection')
 
-const { AssessmentItem, Attainment, CourseUnit, CourseUnitRealisation, StudyRight, StudyRightPrimality, TermRegistration, Person } = require('./db/models')
+const DESTROY = true 
 
-const tableToModel = {
-    assessment_items: AssessmentItem,
-    attainments: Attainment, 
-    course_unit_realisations: CourseUnitRealisation, 
-    course_units: CourseUnit, 
-    persons: Person, 
-    studyrights: StudyRight, 
-    study_right_primalities: StudyRightPrimality, 
-    term_registrations: TermRegistration 
-}
-
-const getSampleOfPersonsFromTestDb = async () => {
-  const { knex } = connection 
+const getIdsOfSuitableStudentsFromTestDb = async () => {
+  const { knex } = sourceConnection
 
   // Settings for sample to be fetched
   // TODO: get these from config and don't hardcode
-  const sampleSize = 20
+  const sampleSize = 100
   const compScienceBachMastProgrammeEducation = 'hy-EDU-114256325-2017'
   const startDateForNewProgrammes = '2017-01-08'
 
-  return await knex
-    .select('persons.*')
+  return (await knex
+  //return await knex
+    .select('persons.id')
     .from('persons')
-    .join('studyrights', 'persons.id', '=', 'studyrights.person_id')
-    .join('term_registrations', 'studyrights.id', '=', 'term_registrations.study_right_id')
-    .where('education_id', compScienceBachMastProgrammeEducation)
-    .andWhere('state', 'ACTIVE')
+    .join('studyrights', 'persons.id', 'studyrights.person_id')
+    .join('term_registrations', 'studyrights.id', 'term_registrations.study_right_id')
+    .join('attainments', builder => 
+      builder.on('attainments.person_id', 'persons.id')
+             .andOn('attainments.study_right_id', 'studyrights.id')
+    )
+    .where('studyrights.education_id', compScienceBachMastProgrammeEducation)
+    .andWhere(
+      builder => builder.whereNotNull(
+        'persons.student_number', 'persons.first_names', 'persons.last_name'
+      )
+    )
     .andWhereRaw("cast(studyrights.valid->>'startDate' as date) >= ?", new Date(startDateForNewProgrammes))
-    .andWhere(builder => builder.whereNotNull('student_number'))
-    .andWhere(builder => builder.whereNotNull('first_names'))
-    .andWhere(builder => builder.whereNotNull('last_name'))
-    .distinctOn('student_number')
+    .distinctOn('persons.student_number')
     .limit(sampleSize)
+    //.toSQL().toNative())
+  ).map(({id}) => id)
+  return [] 
 }
 
-
-const getDataForSelectedStudents = async students => {
-  const { knex } = connection 
-  const studentIds = students.map(({id}) => id)
-
-  const attainmentQuery = knex.select().from('attainments').whereIn('person_id', studentIds).whereNotNull('course_unit_id')
-  const attainments = await attainmentQuery
-  const courseUnits = await knex.select().from('course_units').whereIn('id', attainments.map(({course_unit_id}) => course_unit_id))
-  const assessmentItems = await knex.select().from('assessment_items').whereIn('primary_course_unit_group_id', courseUnits.map(({group_id}) => group_id))
-  const courseUnitRealisations = await knex.select().from('course_unit_realisations').where('assessment_item_ids', '&&', assessmentItems.map(({id}) => id))
-  const teachers = await knex.select().from('persons')
-  .where(builder => builder.whereIn('id', 
-    attainments.reduce((acc, curr) => 
-      [...acc, ...curr.acceptor_persons.map(({personId}) => personId)], []
-    ))
-  .andWhere(builder => builder.whereNotIn('id', studentIds))
-  )
-
-  const studyRights = await knex.select().from('studyrights').whereIn('person_id', studentIds)
-  const studyRightPrimalities = await knex.select().from('study_right_primalities').whereIn('student_id', studentIds)
-
-  // console.log(JSON.stringify(termRegistrations))
-  const persons = [...students, ...teachers]
-
-  return {
-    assessment_items: assessmentItems,
-    attainments: attainments, 
-    course_unit_realisations: courseUnitRealisations, 
-    course_units: courseUnits, 
-    persons: persons, 
-    studyrights: studyRights, 
-    study_right_primalities: studyRightPrimalities, 
-    //term_registrations: termRegistrations
-    // TODO: this fails for some reason when
-    //inserting to db, what
+/**
+  * Nuke stuff from target db in table by preserving rows with given values in given column.
+  * @returns promise?
+*/
+const removeStuff = async (table, column, idsNotToDelete) => {
+  const { knex } = targetConnection
+  if (DESTROY) {
+    const deleted = await knex(table).whereNotIn(column, idsNotToDelete).del()
+    const sizeNow = await knex(table).count()
+    console.log(`Deleted ${deleted} lines from ${table}. Size now: ${sizeNow[0].count}`)
+    return
   }
+  const toBeDeleted = await knex(table).whereNotIn(column, idsNotToDelete).count(column)
+  const toBeKeeped = await knex(table).whereIn(column, idsNotToDelete).count(column)
+  console.log(`Would delete ${toBeDeleted[0].count} rows from ${table} while keeping ${toBeKeeped[0].count} rows.`)
+  return Promise.resolve(toBeKeeped[0].count)
 }
 
-const replaceDataInTable = ({table, data}) => {
-  const model = tableToModel[table]
-  model.truncate()
-  return model.bulkCreate(data)
+const removeAttainmentRelatedData = async students => {
+  const { knex } = sourceConnection
+  const relevantAttainmentsAndCourseUnits = await knex.select('id', 'course_unit_id', 'acceptor_persons').from('attainments').whereIn('person_id', students).whereNotNull('course_unit_id')
+  const courseUnitGroupIds = await knex.select('group_id').from('course_units').whereIn('id', relevantAttainmentsAndCourseUnits.map(({course_unit_id}) => course_unit_id))
+  const relevantAssessmentItems = await knex.select('id').from('assessment_items').whereIn('primary_course_unit_group_id', courseUnitGroupIds.map(({group_id}) => group_id))
+  const relevantCourseUnitRealisations = await knex.select('id').from('course_unit_realisations').where('assessment_item_ids', '&&', relevantAssessmentItems.map(({id}) => id))
+  await Promise.all([
+    removeStuff('attainments', 'id', relevantAttainmentsAndCourseUnits.map(({id}) => id)),
+    removeStuff('course_units', 'group_id', courseUnitGroupIds.map(({group_id}) => group_id)),
+    removeStuff('assessment_items', 'id', relevantAssessmentItems.map(({id}) => id)),
+    removeStuff('course_unit_realisations', 'id', relevantCourseUnitRealisations.map(({id}) => id)),
+    removeStuff('enrolments', 'person_id', students)
+  ])
+  const personIdsOfRelevantTeachers = (await knex.select().from('persons')
+      .where(builder => builder.whereIn('id', 
+        relevantAttainmentsAndCourseUnits.reduce((acc, curr) => 
+          [...acc, ...curr.acceptor_persons.map(({personId}) => personId)], []
+        ))
+      .andWhere(builder => builder.whereNotIn('id', students))
+      )
+  ).map(({id}) => id)
+  return personIdsOfRelevantTeachers
 }
 
-const writeDataToSampleDb = async data => {
-  for (const table_name of Object.keys(data)) {
-    try {
-      const res = await replaceDataInTable({table: table_name, data: data[table_name]})
-      console.log(`Wrote ${res.length} lines to ${table_name}, replacing previous data`)
-    } catch (e) {
-      console.log("Errored with table ", table_name)
-      console.log("error", e)
-    }
-  }
+const removeStudyrightRelatedData = async students => {
+  const { knex } = sourceConnection
+  const relevantEducations = await knex.select('education_id').from('studyrights').whereIn('person_id', students)
+  await Promise.all([
+    removeStuff('educations', 'id', relevantEducations.map(({education_id}) => education_id)),
+    removeStuff('studyrights', 'person_id', students),
+    removeStuff('study_right_primalities', 'student_id', students),
+    removeStuff('term_registrations', 'student_id', students)
+  ])
+}
+
+const removePersons = async (students, teachers) => {
+  await removeStuff('persons', 'id', [...students, ...teachers])
 }
 
 const run = async () => {
-  // Toggle this to write the sample db
-  const WRITE = true
-
-  const students = await getSampleOfPersonsFromTestDb()
-  const dataForSelectedPersons = await getDataForSelectedStudents(students)
-  if (WRITE) {
-    console.log("Creating new sample database")
-    writeDataToSampleDb(dataForSelectedPersons)
-
-    const { knex } = connection 
-    const studentIds = students.map(({id}) => id)
-    const termRegistrations = await knex.select().from('term_registrations')
-      .whereIn('student_id', studentIds)
-
-    console.log(JSON.stringify(termRegistrations[0], null, 2))
-
-    writeDataToSampleDb({
-      term_registrations: [example]
-    })
-
-  } else {
-    console.log("Would write following number of rows to sample db")
-    Object.entries(dataForSelectedPersons).forEach(([table_name, values]) => {
-      console.log(`Table ${table_name}: ${values.length}`)
-    })
-  }
+  const students = await getIdsOfSuitableStudentsFromTestDb()
+  const teachers = await removeAttainmentRelatedData(students)
+  removeStudyrightRelatedData(students)
+  removePersons(students, teachers)
 }
 
 run()
