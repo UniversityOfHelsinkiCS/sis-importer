134545d6a3c36e1cfd16d93f41b29609751319bf
Roni Lindholm <roni.lindholm@helsinki.fi>
2021-06-10 11:26:33 +0300

    Increase request body size limit

--- importer-db-api/src/app.js
+++ importer-db-api/src/app.js
@@ -24,7 +24,7 @@ app.use(Sentry.Handlers.requestHandler())
 app.use(Sentry.Handlers.tracingHandler())
 
 app.use(logger('short'))
-app.use(express.json())
+app.use(express.json({limit: '50mb'}))
 
 app.get('/ping', (req, res) => {
   res.send('pong')
