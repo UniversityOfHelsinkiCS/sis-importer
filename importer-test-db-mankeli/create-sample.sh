38b77685653c0ed81f1962da4870f493218f77df
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-06-17 15:32:04 +0300

    Set db created by test-db-mankeli to match oodikone

--- importer-test-db-mankeli/create-sample.sh
+++ importer-test-db-mankeli/create-sample.sh
@@ -4,14 +4,14 @@
 
 # Create new table
 docker-compose up -d importer-test-db
-docker exec importer-test-db psql -U postgres -c 'DROP DATABASE IF EXISTS "importer-test-db-sample";'
-docker exec importer-test-db psql -U postgres -c 'CREATE DATABASE "importer-test-db-sample" WITH TEMPLATE "importer-db";'
+docker exec importer-test-db psql -U postgres -c 'DROP DATABASE IF EXISTS "sis-importer-db";'
+docker exec importer-test-db psql -U postgres -c 'CREATE DATABASE "sis-importer-db" WITH TEMPLATE "importer-db";'
 
 # Run sampler
 docker-compose up importer-test-db-mankeli
 # TODO: make sample to quit after running
 
-# vacuum! 
-docker exec importer-test-db psql -U postgres importer-test-db-sample -c 'VACUUM FULL;'
+# vacuum!
+docker exec importer-test-db psql -U postgres sis-importer-db -c 'VACUUM FULL;'
 
 # And now we have created sample from importer db!
--- importer-test-db-mankeli/dump-db.sh
+++ importer-test-db-mankeli/dump-db.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-DUMPNAME="importer-test-db-sample-dump.sqz"
+DUMPNAME="sis-importer-db.sqz"
 docker-compose up -d importer-test-db
-docker exec -i importer-test-db pg_dump -Fc -U postgres importer-test-db-sample > "$DUMPNAME"
+docker exec -i importer-test-db pg_dump -Fc -U postgres sis-importer-db > "$DUMPNAME"
