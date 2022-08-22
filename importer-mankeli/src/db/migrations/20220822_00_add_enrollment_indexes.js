bb20121bcd8e8a8f632dc8c54a17f07fc28851a9
Roni Lindholm <roni.lindholm@helsinki.fi>
2022-08-22 13:44:41 +0300

    lintfix

--- importer-mankeli/src/db/migrations/20220822_00_add_enrollment_indexes.js
+++ importer-mankeli/src/db/migrations/20220822_00_add_enrollment_indexes.js
@@ -1,11 +1,9 @@
-const { JSONB } = require('sequelize')
-
 module.exports = {
   up: async queryInterface => {
     return Promise.all([
       queryInterface.addIndex('enrolments', ['person_id']),
       queryInterface.addIndex('enrolments', ['course_unit_id']),
-      queryInterface.addIndex('enrolments', ['course_unit_realisation_id']),
+      queryInterface.addIndex('enrolments', ['course_unit_realisation_id'])
     ])
   },
   down: async () => {}
