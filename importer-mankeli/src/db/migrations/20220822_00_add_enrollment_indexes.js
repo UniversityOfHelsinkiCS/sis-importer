634b73710960863d10138856a89681f2c0d8879b
Roni Lindholm <roni.lindholm@helsinki.fi>
2022-08-22 13:42:31 +0300

    Add indexes for enrollments

---
+++ importer-mankeli/src/db/migrations/20220822_00_add_enrollment_indexes.js
@@ -0,0 +1,12 @@
+const { JSONB } = require('sequelize')
+
+module.exports = {
+  up: async queryInterface => {
+    return Promise.all([
+      queryInterface.addIndex('enrolments', ['person_id']),
+      queryInterface.addIndex('enrolments', ['course_unit_id']),
+      queryInterface.addIndex('enrolments', ['course_unit_realisation_id']),
+    ])
+  },
+  down: async () => {}
+}
