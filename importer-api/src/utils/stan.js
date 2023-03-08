f3122e20f0c95984fc8e0dab3831f4970952a0d6
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-03-08 14:51:23 +0200

    Update nats cid

--- importer-api/src/utils/stan.js
+++ importer-api/src/utils/stan.js
@@ -1,7 +1,7 @@
 const natsStreaming = require('node-nats-streaming')
 const { HOSTNAME, NATS_URI, NATS_TOKEN } = process.env
 
-const stan = natsStreaming.connect('sis-importer-nats', HOSTNAME, {
+const stan = natsStreaming.connect('importer-nats', HOSTNAME, {
   url: NATS_URI,
   token: NATS_TOKEN,
   maxReconnectAttempts: -1,
