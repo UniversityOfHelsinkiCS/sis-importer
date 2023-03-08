c0beed7eb25f115161a7ea40e30b7bf05b64c0ee
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-03-08 15:11:13 +0200

    Update importer-mankeli db config

--- importer-mankeli/src/config/index.js
+++ importer-mankeli/src/config/index.js
@@ -4,21 +4,7 @@ module.exports.MIGRATIONS_LOCK = 'MIGRATIONS_LOCK'
 
 const { DB_USERNAME, DB_PASSWORD, DB_PORT, DB_HOST, DB_DATABASE } = process.env
 
-module.exports.DB_CONFIG = {
-  dialect: 'postgres',
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 10000,
-    idle: 300000000
-  },
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  port: DB_PORT,
-  host: DB_HOST,
-  database: DB_DATABASE,
-  logging: false
-}
+module.exports.DB_CONNECTION_STRING = `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?targetServerType=primary&ssl=true`
 
 module.exports.IS_DEV = process.env.NODE_ENV === 'development'
 
