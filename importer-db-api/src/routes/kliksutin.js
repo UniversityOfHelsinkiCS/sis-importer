4ebddf29da84b9646ec58f241d830de028421eb3
Henri Remonen <henri@remonen.fi>
2023-04-24 13:52:19 +0300

    Update kliksutin endpoint to maybe give better data of the courses

--- importer-db-api/src/routes/kliksutin.js
+++ importer-db-api/src/routes/kliksutin.js
@@ -5,15 +5,26 @@ const models = require('../models/index.js')
 
 const router = express.Router()
 
+const validRealisationTypes = [
+  'urn:code:course-unit-realisation-type:teaching-participation-lab',
+  'urn:code:course-unit-realisation-type:teaching-participation-online',
+  'urn:code:course-unit-realisation-type:teaching-participation-field-course',
+  'urn:code:course-unit-realisation-type:teaching-participation-project',
+  'urn:code:course-unit-realisation-type:teaching-participation-lectures',
+  'urn:code:course-unit-realisation-type:teaching-participation-small-group',
+  'urn:code:course-unit-realisation-type:teaching-participation-seminar',
+  'urn:code:course-unit-realisation-type:independent-work-project', // ship these to the norppa side even if they arent widely used
+]
+
 router.get('/courses/:personId', async (req, res) => {
   const { personId: teacherId } = req.params
 
-  const teacherCourses = await models.CourseUnit.findAll({
+  const teacherCourses = await models.CourseUnitRealisation.findAll({
     where: {
       responsibilityInfos: {
         [Op.contains]: [{ personId: teacherId }], // note that '{ personId: teacherId }' would not work. In pg, array containment is more like checking for union
       },
-      validityPeriod: {
+      activityPeriod: {
         endDate: {
           [Op.gt]: new Date(),
         },
@@ -21,6 +32,9 @@ router.get('/courses/:personId', async (req, res) => {
           [Op.lt]: addMonths(new Date(), 6),
         },
       },
+      courseUnitRealisationTypeUrn: {
+        [Op.in]: validRealisationTypes
+      }
     },
   })
 
