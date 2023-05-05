0f00b68880beadfc6e4948af27454a85f89767ce
Henri Remonen <henri@remonen.fi>
2023-05-05 10:18:59 +0300

    Include courseUnits in the query

--- importer-db-api/src/routes/kliksutin.js
+++ importer-db-api/src/routes/kliksutin.js
@@ -19,11 +19,9 @@ const validRealisationTypes = [
 const relevantAttributes = {
   courseUnit: [
     'id',
-    'groupId',
     'code',
-    'organisations',
-    'completionMethods',
     'responsibilityInfos',
+    'completionMethods',
     'name',
     'validityPeriod',
   ],
@@ -31,17 +29,62 @@ const relevantAttributes = {
     'id',
     'name',
     'nameSpecifier',
+    'assessmentItemIds',
     'activityPeriod',
     'courseUnitRealisationTypeUrn',
     'responsibilityInfos',
   ],
+  assessmentItem: ['id', 'name', 'nameSpecifier', 'assessmentItemType', 'organisations', 'primaryCourseUnitGroupId'],
+}
+
+const timeTillCourseStart = 6
+
+const addCourseUnitsToRealisations = async courseUnitRealisations => {
+  const assessmentItemIds = courseUnitRealisations.flatMap(c => c.assessmentItemIds)
+
+  const assessmentItemsWithCrap = await models.AssessmentItem.findAll({
+    attributes: relevantAttributes.assessmentItem,
+    where: {
+      id: {
+        [Op.in]: assessmentItemIds,
+      },
+    },
+    include: [
+      {
+        model: models.CourseUnit,
+        attributes: relevantAttributes.courseUnit,
+        as: 'courseUnit',
+        required: true,
+      }
+    ],
+  })
+
+  const assessmentItems = assessmentItemsWithCrap.filter(aItem => (aItem?.courseUnit.completionMethods.find(method => method.assessmentItemIds.includes(aItem.id))))
+
+  const realisationsWithCourseUnits = courseUnitRealisations.map((r) => {
+    const realisation = r.get({ plain: true })
+  
+    const courseUnits = assessmentItems
+      .filter(assessmentItem =>
+        realisation.assessmentItemIds.includes(assessmentItem.id)
+      )
+      .map(assessmentItem => {
+        const courseUnit = assessmentItem.get({ plain: true }).courseUnit
+        return courseUnit
+      })
+  
+    return {
+      ...realisation,
+      courseUnits,
+    }
+  })
+
+  return realisationsWithCourseUnits
 }
 
 
 router.get('/courses/:personId', async (req, res) => {
   const { personId: teacherId } = req.params
-
-  const timeTillCourseStart = 6
 
   const courseUnitRealisations = await models.CourseUnitRealisation.findAll({
     attributes: relevantAttributes.courseUnitRealisation,
@@ -63,23 +106,9 @@ router.get('/courses/:personId', async (req, res) => {
     },
   })
 
-  const courseUnits = await models.CourseUnit.findAll({
-    where: {
-      responsibilityInfos: {
-        [Op.contains]: [{ personId: teacherId }], // note that '{ personId: teacherId }' would not work. In pg, array containment is more like checking for union
-      },
-      validityPeriod: {
-        endDate: {
-          [Op.gt]: new Date(),
-        },
-        startDate: {
-          [Op.lt]: addMonths(new Date(), timeTillCourseStart),
-        },
-      },
-    },
-  })
+  const courseUnitRealisationsWithCourseUnits = await addCourseUnitsToRealisations(courseUnitRealisations)
 
-  res.send(courseUnitRealisations)
+  res.send(courseUnitRealisationsWithCourseUnits)
 })
 
 module.exports = router
