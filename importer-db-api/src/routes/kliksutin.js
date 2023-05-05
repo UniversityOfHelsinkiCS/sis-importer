0106666e84cf45b348caf066306b72f17e2e4c57
Henri Remonen <henri@remonen.fi>
2023-05-05 08:43:31 +0300

    Added relevant attributes lists

--- importer-db-api/src/routes/kliksutin.js
+++ importer-db-api/src/routes/kliksutin.js
@@ -16,10 +16,35 @@ const validRealisationTypes = [
   'urn:code:course-unit-realisation-type:independent-work-project',
 ]
 
+const relevantAttributes = {
+  courseUnit: [
+    'id',
+    'groupId',
+    'code',
+    'organisations',
+    'completionMethods',
+    'responsibilityInfos',
+    'name',
+    'validityPeriod',
+  ],
+  courseUnitRealisation: [
+    'id',
+    'name',
+    'nameSpecifier',
+    'activityPeriod',
+    'courseUnitRealisationTypeUrn',
+    'responsibilityInfos',
+  ],
+}
+
+
 router.get('/courses/:personId', async (req, res) => {
   const { personId: teacherId } = req.params
 
-  const teacherCourses = await models.CourseUnitRealisation.findAll({
+  const timeTillCourseStart = 6
+
+  const courseUnitRealisations = await models.CourseUnitRealisation.findAll({
+    attributes: relevantAttributes.courseUnitRealisation,
     where: {
       responsibilityInfos: {
         [Op.contains]: [{ personId: teacherId }], // note that '{ personId: teacherId }' would not work. In pg, array containment is more like checking for union
@@ -29,7 +54,7 @@ router.get('/courses/:personId', async (req, res) => {
           [Op.gt]: new Date(),
         },
         startDate: {
-          [Op.lt]: addMonths(new Date(), 6),
+          [Op.lt]: addMonths(new Date(), timeTillCourseStart),
         },
       },
       courseUnitRealisationTypeUrn: {
