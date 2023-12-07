94863d377577e4741524b956f6bd9a29c60d04cd
Veikkosuhonen <vesuvesuh@gmail.com>
2023-12-07 15:04:56 +0200

    Fix the debugger func

--- importer-mankeli/src/debug/curEnrolmentsDebug.js
+++ importer-mankeli/src/debug/curEnrolmentsDebug.js
@@ -11,7 +11,7 @@ const curId = 'hy-opt-cur-2324-f0c0d370-0087-4bb9-93aa-387e76bd11f2'
  * @param {*} enrolment
  */
 const matcher = enrolment => {
-  return enrolment.courseUnitRealisationId === curId
+  return true // enrolment.courseUnitRealisationId === curId
 }
 
 const debugHandler = enrolment => {
@@ -22,8 +22,9 @@ const debugHandler = enrolment => {
 module.exports = {
   channel,
   handler: msg => {
-    if (matcher(msg)) {
-      debugHandler(msg)
+    const entitiesOfInterest = msg.entities.filter(matcher)
+    if (entitiesOfInterest.length > 0) {
+      entitiesOfInterest.forEach(debugHandler)
     }
     return msg
   }
