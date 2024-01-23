8bdd1239b2cdae0c64a4c91873d371b37edc3b08
ShootingStar91 <ttu91@hotmail.com>
2024-01-23 19:10:39 +0200

    Fix bulkCreate failing all entities in case of one error. Now entities are re-ran individually if an error appears

--- importer-mankeli/src/utils/db.js
+++ importer-mankeli/src/utils/db.js
@@ -4,10 +4,26 @@ const { connection } = require('../db/connection')
 const getColumnsToUpdate = (model, keys) => Object.keys(model.rawAttributes).filter(a => !keys.includes(a))
 
 const bulkCreate = async (model, entities, transaction, properties = ['id']) => {
+  try {
   await model.bulkCreate(entities, {
     updateOnDuplicate: getColumnsToUpdate(model, properties),
     transaction
   })
+  } catch (error) {
+    // If one fails on bulkCreate, re-do them individually and report the individual failures.
+    for (const entity of entities) {
+      try {
+        await model.upsert(entity, {
+          updateOnDuplicate: getColumnsToUpdate(model, properties),
+          transaction
+        })
+      } catch (e) {
+        console.log("Single-entity upsert failed: ", JSON.stringify(e, null, 2))
+        console.log("Entity:")
+        console.log(JSON.stringify(entity, null, 2))
+      }
+    }
+  }
 }
 
 const bulkDelete = async (model, entities, transaction, property = 'id') => {
