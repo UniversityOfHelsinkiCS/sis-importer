1014e3f5e35ce451d26d77ae610e722155c30108
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-05-29 11:04:26 +0300

    Add person endpoint for grapa application

--- importer-db-api/src/routes/grapa/config.js
+++ importer-db-api/src/routes/grapa/config.js
@@ -1,5 +1,5 @@
 const relevantAttributes = {
-  persons: ['id', 'eduPersonPrincipalName', 'preferredLanguageUrn', 'firstName', 'lastName', 'primaryEmail'],
+  persons: ['id', 'edu_person_principal_name', 'preferred_language_urn', 'first_names', 'last_name', 'primary_email', 'employee_number'],
 }
 
 module.exports = {
--- importer-db-api/src/routes/grapa/grapa.js
+++ importer-db-api/src/routes/grapa/grapa.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const models = require('../../models')
-
 const { relevantAttributes } = require('./config')
+const models = require('../../models')
+const { sequelize } = require('../../config/db')
 
 const router = express.Router()
 
@@ -11,14 +11,24 @@ grapaRouter.get('/persons', async (req, res) => {
   const { limit, offset } = req.query
   if (!limit || !offset) return res.sendStatus(400)
 
-  const persons = await models.Person.findAll({
-    attributes: relevantAttributes.persons,
-    order: [['id', 'DESC']],
-    limit,
-    offset,
-  })
+  const personsWithStudyRightOrEmployeeNumber = await sequelize.query(
+    `SELECT ${relevantAttributes.persons.map(attr => `P.${attr}`)}
+      FROM persons P
+      LEFT JOIN person_study_rights_view psr ON psr.person_id = P.id
+      WHERE has_study_right IS TRUE OR employee_number IS NOT NULL
+      ORDER BY P.id DESC
+      LIMIT :limit OFFSET :offset`,
+    {
+      replacements: {
+        limit,
+        offset
+      },
+      mapToModel: true,
+      model: models.Person,
+    }
+  )
 
-  res.send(persons)
+  res.send(personsWithStudyRightOrEmployeeNumber)
 })
 
 router.use('/', grapaRouter)
