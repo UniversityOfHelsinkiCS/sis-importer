13410c8a7eec626cc0f1907bf9fe95d3b7aea4a0
Jan Rundt <jan.rundt@funidata.fi>
2024-07-30 11:23:16 +0300

    OOD-12-13-40: fd can also define sentryDSN if needed

--- importer-db-api/src/utils/sentry.js
+++ importer-db-api/src/utils/sentry.js
@@ -2,10 +2,12 @@ const Sentry = require('@sentry/node')
 const Tracing = require('@sentry/tracing') // eslint-disable-line
 
 const initializeSentry = app => {
-  if (process.env.NODE_ENV !== 'production' || process.env.SERVICE_PROVIDER === 'fd') return
+  if (process.env.NODE_ENV !== 'production' || (process.env.SERVICE_PROVIDER === 'fd' && !process.env.SENTRY_DSN)) return
+
+  const sentryDSN = process.env.SENTRY_DSN || 'https://eacaccbb66a62f268b3241ddc4da8519@toska.cs.helsinki.fi/9'
 
   Sentry.init({
-    dsn: 'https://eacaccbb66a62f268b3241ddc4da8519@toska.cs.helsinki.fi/9',
+    dsn: sentryDSN,
     integrations: [new Sentry.Integrations.Http({ tracing: true }), new Tracing.Integrations.Express({ app })],
     tracesSampleRate: 1.0
   })
--- importer-mankeli/src/utils/sentry.js
+++ importer-mankeli/src/utils/sentry.js
@@ -1,10 +1,12 @@
 const Sentry = require('@sentry/node')
 
 const initializeSentry = () => {
-  if (process.env.NODE_ENV !== 'production' || process.env.SERVICE_PROVIDER === 'fd') return
+  if (process.env.NODE_ENV !== 'production' || (process.env.SERVICE_PROVIDER === 'fd' && !process.env.SENTRY_DSN)) return
+
+  const sentryDSN = process.env.SENTRY_DSN || 'https://eacaccbb66a62f268b3241ddc4da8519@toska.cs.helsinki.fi/9'
 
   Sentry.init({
-    dsn: 'https://eacaccbb66a62f268b3241ddc4da8519@toska.cs.helsinki.fi/9'
+    dsn: sentryDSN
   })
 }
 
