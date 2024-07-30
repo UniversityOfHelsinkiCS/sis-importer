eae8747cb0c4401371afd4587bbdf922c7f13807
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2024-07-30 13:14:30 +0300

    Fix lint errors

--- importer-db-api/src/utils/sentry.js
+++ importer-db-api/src/utils/sentry.js
@@ -2,7 +2,8 @@ const Sentry = require('@sentry/node')
 const Tracing = require('@sentry/tracing') // eslint-disable-line
 
 const initializeSentry = app => {
-  if (process.env.NODE_ENV !== 'production' || (process.env.SERVICE_PROVIDER === 'fd' && !process.env.SENTRY_DSN)) return
+  if (process.env.NODE_ENV !== 'production' || (process.env.SERVICE_PROVIDER === 'fd' && !process.env.SENTRY_DSN))
+    return
 
   const sentryDSN = process.env.SENTRY_DSN || 'https://eacaccbb66a62f268b3241ddc4da8519@toska.cs.helsinki.fi/9'
 
