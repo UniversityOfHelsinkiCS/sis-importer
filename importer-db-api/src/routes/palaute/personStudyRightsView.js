a3a4f8508db10f14d4e4ab52c9e21faa62c2f62a
Aleksandr Tereshchenko <aleksandr.tereshch@gmail.com>
2024-10-07 10:14:52 +0300

    Also consider modification_ordinal in personStudyRightView

--- importer-db-api/src/routes/palaute/personStudyRightsView.js
+++ importer-db-api/src/routes/palaute/personStudyRightsView.js
@@ -34,7 +34,7 @@ const createPersonStudyRightsView = async () => {
           SELECT * from studyrights
           WHERE person_id = P.id
           AND (snapshot_date_time <= NOW() OR snapshot_date_time IS NULL)
-          ORDER BY snapshot_date_time DESC
+          ORDER BY snapshot_date_time DESC NULLS LAST, modification_ordinal DESC
           NULLS LAST
           LIMIT 1
         ) S, educations E
