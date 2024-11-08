594c66ba64e8e96a02b2eb26d6b2dbc902becb70
Aleksandr Tereshchenko <aleksandr.tereshchenko@helsinki.fi>
2024-11-08 12:16:37 +0200

    Try agother option with nats url

--- importer-api/src/utils/stan.js
+++ importer-api/src/utils/stan.js
@@ -2,8 +2,7 @@ const natsStreaming = require('node-nats-streaming')
 const { HOSTNAME, NATS_TOKEN } = process.env
 
 const stan = natsStreaming.connect('importer-nats', HOSTNAME, {
-  url: 'nats-importer',
-  port: 4222,
+  url: 'nats-importer:4222',
   token: NATS_TOKEN,
   maxReconnectAttempts: -1,
   waitOnFirstConnect: true,
