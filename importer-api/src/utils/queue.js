a0576b409175c476e8926e16726de820a76a8548
Matti Räsänen <matti.rasanen@funidata.fi>
2025-10-28 14:01:04 +0200

    [importer-api] reverted queue changes

--- importer-api/src/utils/queue.js
+++ importer-api/src/utils/queue.js
@@ -1,39 +1,10 @@
 const { Queue, QueueEvents } = require('bullmq')
 
-const {
-  REDIS_HOST,
-  REDIS_PORT,
-  SERVICE_PROVIDER,
-  QUEUE_COMPLETE_COUNT,
-  QUEUE_COMPLETE_AGE,
-  QUEUE_FAIL_COUNT,
-  QUEUE_FAIL_AGE
-} = require('../config')
+const { REDIS_HOST, REDIS_PORT } = require('../config')
 
 const connection = {
   host: REDIS_HOST,
   port: REDIS_PORT
-}
-
-const defaultJobOptions = {
-  removeOnComplete: {
-    count: 1000
-  },
-  removeOnFail: {
-    count: 5000
-  },
-  attempts: 0
-}
-const fdJobOptions = {
-  ...defaultJobOptions,
-  removeOnComplete: {
-    age: QUEUE_COMPLETE_AGE,
-    count: QUEUE_COMPLETE_COUNT
-  },
-  removeOnFail: {
-    age: QUEUE_FAIL_AGE,
-    count: QUEUE_FAIL_COUNT
-  }
 }
 
 const queue = new Queue('importer-queue', {
@@ -42,7 +13,15 @@ const queue = new Queue('importer-queue', {
     max: 10000,
     duration: 1000
   },
-  defaultJobOptions: SERVICE_PROVIDER !== 'fd' ? defaultJobOptions : fdJobOptions
+  defaultJobOptions: {
+    removeOnComplete: {
+      count: 1000
+    },
+    removeOnFail: {
+      count: 5000
+    },
+    attempts: 0
+  }
 })
 
 const queueEvents = new QueueEvents('importer-queue', { connection })
