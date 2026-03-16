42ef332f563d0a2f60e2a9c6ae6bc37b4f9b893d
Matti Luukkainen <mluukkai@iki.fi>
2026-03-16 13:45:51 +0200

    remove fd

--- importer-db-api/src/utils/sentry.js
+++ importer-db-api/src/utils/sentry.js
@@ -1,9 +1,9 @@
 const Sentry = require('@sentry/node')
 const { nodeProfilingIntegration } = require('@sentry/profiling-node')
 
-const { serviceProvider, configSentryDSN, nodeEnv } = require('../config')
+const { configSentryDSN, nodeEnv } = require('../config')
 
-if (nodeEnv === 'production' && (serviceProvider !== 'fd' || configSentryDSN)) {
+if (nodeEnv === 'production' && configSentryDSN) {
   Sentry.init({
     dsn: configSentryDSN || 'https://eacaccbb66a62f268b3241ddc4da8519@toska.cs.helsinki.fi/9',
     integrations: [nodeProfilingIntegration()],
