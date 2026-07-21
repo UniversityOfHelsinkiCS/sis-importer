55bb931079ed3cd4a366bf9dc22a5ff2242299ad
smxsm <74610181+e11-0a@users.noreply.github.com>
2026-07-21 11:55:54 +0300

    Fix accidentally inverted condition

--- importer-api/src/utils/index.js
+++ importer-api/src/utils/index.js
@@ -10,10 +10,12 @@ const retry = async (func, params, attempts = 6) => {
     } catch (err) {
       if (i === attempts) {
         logger.error({ message: 'Calling function failed', meta: err.stack })
+        throw err
       }
       if (err.response.status === 403) {
         // Forbidden. We won't have access by bashing our head against it
         logger.error({ message: 'Forbidden endpoint', meta: err.stack })
+        throw err
       }
       logger.info(`Retrying ${i}/${attempts - 1}`)
       await sleep(i * 1000)
@@ -27,6 +29,7 @@ const request = async (instance, path, attemps = 6) => {
     return res.data
   } catch (err) {
     logger.error(`Request failed for ${path}`, err)
+    throw err
   }
 }
 
